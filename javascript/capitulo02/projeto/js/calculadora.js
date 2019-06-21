2ffad92e0cd73501f64e12586d1a950346ef29ba
Wellington Felix <wellington.felix@avancoinfo.com.br>
2019-06-21 13:44:29 -0300

    trabalhando no projeto calculadora.

--- javascript/capitulo02/projeto/js/calculadora.js
+++ javascript/capitulo02/projeto/js/calculadora.js
@@ -1,9 +1,38 @@
-function recuperaValorDoBotao(valor) {
+var insereValorNoResultado = function(valor) {
 
-}
+};
+
+var pegaValorDoBotao = function(valor) {
+    debugger;
+    if (isNaN(valor)) {
+        operador = valor;
+
+        switch (operador) {
+            case '+':
+                break;
+
+            case '-':
+                break;
+
+            case '*':
+                break;
+
+            case '/':
+                break;
+        }
+    } else {
+        if (!bandeira) {
+            n1 = parseInt(valor);
+
+            bandeira = true;
+        } else {
+            n2 = parseInt(valor);
+        }
+    }
+};
 
 var n1 = undefined;
-
-n1 = 0;
-
-document.getElementById('tela').value = n1;
+var n2 = undefined;
+var operador = undefined;
+var resultado = undefined;
+var bandeira = false;
