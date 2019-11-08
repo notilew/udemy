78f84629341f12dba974bfea69f4bf5af8ea197f
Wellington Felix <wellington.felix@avancoinfo.com.br>
2019-11-08 13:22:38 -0300

    trabalhando no desafio 02

--- javascript/capitulo10/desafio02/js/controller.js
+++ javascript/capitulo10/desafio02/js/controller.js
@@ -1,7 +1,15 @@
 ((window, document) => {
     'use strict';
 
-    var timestamp = 0;
+    const UMA_HORA = 60 * 60 * 1000;
+    const UM_MINUTO = 60 * 1000;
+    const UM_SEGUNDO = 10 * 100;
+
+    var timestampInicio = 0;
+    var timestampPausa = 0;
+    var timestampDecorrido = 0;
+    var iniciado = false;
+    var id = undefined;
 
     var cronometro = document.querySelector('#cronometro');
     var iniciar = document.querySelector('#iniciar');
@@ -13,25 +21,80 @@
     });
 
     iniciar.addEventListener('click', () => {
-        timestamp = Date.now();
-        iniciarCronometro();
+        if (!iniciado) {
+            iniciado = true;
+            timestampInicio = Date.now();
+
+            pausar.textContent = 'Pausar';
+
+            iniciarCronometro();
+        }
+    });
+
+    pausar.addEventListener('click', (e) => {
+        if (cronometro.value !== '00:00:00:000') {
+            if (iniciado) {
+                iniciado = false;
+                timestampPausa = Date.now();
+                timestampDecorrido += timestampPausa - timestampInicio;
+
+                e.target.textContent = 'Continuar';
+
+                window.clearInterval(id);
+            } else {
+                iniciado = true;
+                timestampInicio = Date.now();
+
+                e.target.textContent = 'Pausar';
+
+                iniciarCronometro();
+            }
+        }
+    });
+
+    resetar.addEventListener('click', () => {
+        timestampInicio = 0;
+        timestampPausa = 0;
+        timestampDecorrido = 0;
+        iniciado = false;
+
+        pausar.textContent = 'Pausar';
+
+        window.clearInterval(id);
+
+        cronometro.value = '00:00:00:000';
     });
 
     function iniciarCronometro() {
-        window.setInterval(() => {
-            cronometro.value = retornaTempoFormatado((Date.now() - timestamp));
+        id = window.setInterval(() => {
+            var timestampAtual = Date.now();
+            var diff = timestampAtual - timestampInicio;
+            cronometro.value = retornaTempoFormatado(diff + timestampDecorrido);
         }, 1);
     }
 
-    function retornaTempoFormatado(timestamp) {
-        var tempo = new Date(timestamp);
+    function retornaTempoFormatado(tempo) {
+        if (tempo < UM_SEGUNDO) {
+            return '00:00:00:' + tempo;
+        } else if (tempo < UM_MINUTO) {
+            var segundos = formataTempo(window.parseInt(tempo / UM_SEGUNDO));
+            var milisegundos = tempo - (segundos * UM_SEGUNDO);
 
-        var horas = formataTempo(tempo.getHours());
-        var minutos = formataTempo(tempo.getMinutes());
-        var segundos = formataTempo(tempo.getSeconds());
-        var milisegundos = formataTempo(tempo.getMilliseconds());
+            return '00:00:' + segundos + ':' + milisegundos;
+        } else if (tempo < UMA_HORA) {
+            var minutos = formataTempo(window.parseInt(tempo / UM_MINUTO));
+            var segundos = formataTempo(window.parseInt(tempo / UM_SEGUNDO - (minutos * 60)));
+            var milisegundos = tempo - (segundos * UM_SEGUNDO) - (minutos * 60 * UM_SEGUNDO);
 
-        return horas + ':' + minutos + ':' + segundos + ':' + milisegundos;
+            return '00:' + minutos + ':' + segundos + ':' + milisegundos;
+        } else {
+            var horas = formataTempo(window.parseInt(tempo / UMA_HORA));
+            var minutos = formataTempo(window.parseInt(tempo / UM_MINUTO) - 60);
+            var segundos = formataTempo(window.parseInt(tempo / UM_SEGUNDO) - (minutos * 60));
+            var milisegundos = tempo - (segundos * UM_SEGUNDO) - (minutos * 60 * UM_SEGUNDO);
+
+            return horas + ':' + minutos + ':' + segundos + ':' + milisegundos;
+        }
     }
 
     function formataTempo(tempo) {
