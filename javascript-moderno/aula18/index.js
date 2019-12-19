de15eaaaeb021ebab81c672285ae0567cba38427
Wellington Felix <wellington.felix@avancoinfo.com.br>
2019-12-19 13:59:51 -0300

    adicionando novos exercícios.

--- javascript-moderno/aula18/index.js
+++ javascript-moderno/aula18/index.js
@@ -20,10 +20,39 @@ const senha = '123456';
 
 const logar = (email === 'wellington.felix@avancoinfo.com.br' && senha === '123456');
 
-if (logar) {
-    console.log('vc logou corretamente!');
+console.log(logar);
+
+console.log(!true);
+
+console.log(!!true);
+
+// valores falsos em JavaScript.
+// false, 0, '', "", ``, null, undefined, NaN
+
+// verificando retorno de camparações lógicas.
+console.log(NaN && true);
+console.log(0 && true);
+console.log('wellington' && true && '');
+console.log(0 || null || undefined || 'wellington felix' || true);
+
+// utilizando valores lógicos para realizar uma condicional - short-circuit (curto-circuito).
+function msg() {
+    return 'a mensagem é...';
 }
+
+let exibir;
+
+exibir = false;
+
+console.log(`avaliação curto-circuito ${exibir && msg()}`);
+
+exibir = true;
+
+console.log(`avaliação curto-circuito ${exibir && msg()}`);
+
+
+
 /**
  * Observações:
- * Operadores de comparações lógicas retornam um boolean como resultado de uma expressão.
+ * Operadores de comparações lógicas podem retornar um boolean como resultado de uma expressão ou o próprio valor literal da expressão.
  */
