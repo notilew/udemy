dc66582a12d932b68ca540e006828006f5b85f0b
Wellington Felix <wellington.felix@avancoinfo.com.br>
2020-01-17 13:49:01 -0300

    adicionando novos exercícios.

--- javascript-moderno/aula26/exercicios/modelo/assets/js/main.js
+++ javascript-moderno/aula26/exercicios/modelo/assets/js/main.js
@@ -1,54 +1,38 @@
 function insereElementoNoDOM(elemento) {
-    debugger;
-    if (elemento) {
+    let div = document.querySelector('div#main div');
+    
+    if (!div) {
         const main = document.querySelector('div#main');
-        let div = document.querySelector('div#main > div');
 
-        if (!div) {
-            div = criaElemento({ tag: 'div', textContent: '' });
-        }
-
+        div = document.createElement('div');
+        main.appendChild(div);
+    } else {
         div.appendChild(elemento);
     }
 }
 
-function criaElemento(objeto, posicao) {debugger;
-    let div;
-
-    if (posicao === 0) {
-        div = document.createElement('div');
-    }
-
+function criaElemento(objeto) {
     const elemento = document.createElement(objeto.tag);
+
     elemento.textContent = objeto.textContent.toUpperCase()
 
-    div.appendChild(elemento);
-
-    return div;
+    return elemento;
 }
 
 function escopo() {
     const elementos = [
-        {
-            tag: 'p',
-            textContent: 'frase 1'
-        },
-        {
-            tag: 'div',
-            textContent: 'frase 2'
-        },
-        {
-            tag: 'footer',
-            textContent: 'frase 3'
-        },
-        {
-            tag: 'section',
-            textContent: 'frase 4'
-        },
+        { tag: 'p', textContent: 'frase 1' },
+        { tag: 'div', textContent: 'frase 2' },
+        { tag: 'footer', textContent: 'frase 3' },
+        { tag: 'section', textContent: 'frase 4' },
     ];
 
+    insereElementoNoDOM();
+
     for (let i = 0; i < elementos.length; i++) {
-        console.log(criaElemento(elementos[i], i));
+        const elemento = criaElemento(elementos[i]);
+
+        insereElementoNoDOM(elemento);
     }
 }
 
